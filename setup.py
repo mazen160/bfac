493aaf0cca250b40e5a88a6f2a2f52115ba81fdb
EdOverflow <diopterasec@gmail.com>
2016-11-18 17:36:41 +0100

    :arrow_up: Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, Extension
 
 setup(
   name = 'bfac',
   packages=['bfac'],
   version = '1.1',
-  scripts=['bfac'],
+  scripts=['bfac/bfac'],
   description = "An automated tool that checks for backup artifacts that may discloses the web-application's source code.",
-  long_description=open('README.txt').read(),
+  long_description=open('README.md').read(),
   author = 'Mazin Ahmed',
   author_email = 'mazin@mazinahmed.net',
   url = 'https://github.com/mazen160/bfac',
-  keywords = ['backup'],
-  install_requires = ['argparse', 'request'],
+  keywords = ['backup','artifacts','checker','web scanner','web vulnerability scanner','bfac'],
+  install_requires = ['argparse', 'requests'],
   license="GPL-3.0"
 )
