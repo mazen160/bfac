a56f99b23af9fcf013fff5c534a4c20ef1445ede
EdOverflow <EdOverflow@users.noreply.github.com>
2016-11-22 12:58:45 +0100

    Add setup.py (#2)
    * :heavy_plus_sign: Add setup.py
    
    * :heavy_plus_sign: Add .gitignore
    
    * :arrow_up: Update setup.py
    
    * :heavy_multiplication_x: Remove .gitignore file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+
+setup(
+  name = 'bfac',
+  packages=['bfac'],
+  version = '1.1',
+  scripts=['bfac/bfac'],
+  description = "An automated tool that checks for backup artifacts that may discloses the web-application's source code.",
+  long_description=open('README.md').read(),
+  author = 'Mazin Ahmed',
+  author_email = 'mazin@mazinahmed.net',
+  url = 'https://github.com/mazen160/bfac',
+  keywords = ['backup','artifacts','checker','web scanner','web vulnerability scanner','bfac'],
+  install_requires = ['argparse', 'requests'],
+  license="GPL-3.0"
+)
